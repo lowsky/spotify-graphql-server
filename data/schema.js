5932b03ee5241356bbfaed0a57b00fa6ba42fca4
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2016-12-22 14:53:51 +0100

    add types: Artist, Album, Track

--- data/schema.js
+++ data/schema.js
@@ -8,6 +8,25 @@ const schema = buildSchema(`
 type Query {
   # Just returns "Hello world!"
   hi(message: String = "Hi"): String
+  queryArtists(byName: String = "Red Hot Chili Peppers"): [Artist]
+}
+type Artist {
+  name: String!
+  id: ID
+  image: String
+  albums(limit: Int = 10): [Album]
+}
+type Album {
+  name: String
+  id: ID
+  image: String
+  tracks: [Track]
+}
+type Track {
+  name: String!
+  artists: [Artist]
+  preview_url: String
+  id: ID
 }
 `);
 
