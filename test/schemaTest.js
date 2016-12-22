3b49c890e8fa5d29b4cedd1d29e88702b53f3252
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2016-12-22 15:43:02 +0100

    add simpletest to run a query against the mock server

---
+++ test/schemaTest.js
@@ -0,0 +1,33 @@
+import {mockServer, MockList} from 'graphql-tools';
+
+import schema from '../data/schema';
+
+let cnt = 0;
+
+const simpleMockServer = mockServer(schema, {
+    String: () => 'loremipsum ' + (cnt++),
+    Album: () => {
+        return {
+            name: () => {return 'Album#1'}
+        };
+    }
+});
+
+const result = simpleMockServer.query(`{
+    queryArtists(byName:"Marilyn Manson") {
+        name
+        albums {
+            name
+            tracks {
+                name
+                artists { name }
+            }
+        }
+    }
+}`);
+
+result.then(data => {
+    console.log('data: ', JSON.stringify(data, '  ', 1));
+}).catch(error => {
+    console.log('error: ', error);
+});
