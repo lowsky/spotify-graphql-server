97c0c9e907fc5942a7db1f89b15dafcc9aa8df6f
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2017-01-04 18:11:52 +0100

    add proper information to start page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var packageJson = require('../package.json');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Spotify Graphql Server', version: packageJson.version });
 });
 
 module.exports = router;
